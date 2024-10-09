2b40f18f5677606f7f109b215b4f87be85b68de3
Filipp <109096923+immxrtalbeast@users.noreply.github.com>
2024-10-09 17:16:03 +0300

    Update laba1.py

--- laba1.py
+++ laba1.py
@@ -1,8 +1,5 @@
 import math
-pi = 3.14
 x = input('Введите x: ')
 y = input("Введите y: ")
-chisl = 3 * ((math.cos(int(x) - pi/6)) ** 2)
-snam = 0.5 + math.sin(int(y)**2)
-print(chisl/snam)
+print((3 * ((math.cos(int(x) - math.pi/6)) ** 2))/( 0.5 + math.sin(int(y)**2)))
 input()
