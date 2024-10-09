3da590d11a0a3f6357d5b3097f89e737912acf22
Filipp <109096923+immxrtalbeast@users.noreply.github.com>
2024-10-09 19:13:53 +0300

    Update laba1.py

--- laba1.py
+++ laba1.py
@@ -1,5 +1,5 @@
 import math
 x = input('Введите x: ')
 y = input("Введите y: ")
-print((3 * ((math.cos(int(x) - math.pi/6)) ** 2))/( 0.5 + math.sin(int(y)**2)))
+print((3 * ((math.cos(float(x) - math.pi/6)) ** 2))/( 0.5 + math.sin(float(y)**2)))
 input()
